ed6b208ae9137d9905224eb2367923ce4353d7d5
Thomas vilte <viltetomas2003@gmail.com>
2022-04-22 21:04:35 -0300

    Codigo actualizado

--- manejoDeErrores/excepciones.py
+++ manejoDeErrores/excepciones.py
@@ -1,18 +1,41 @@
-resultado = None
+def suma(num1, num2):
+    return num1 + num2
 
-try:
-    a = int(input('Ingrese un numero: '))
-    b = int(input('Ingrese un numero: '))
-    resultado = a/b
+def resta(num1, num2):
+    return num1 - num2
 
-except ZeroDivisionError as e:
-    print(f'ZeroDivisionError - Ocurrio un error: {e}, {type(e)}')   
+def multiplicacion(num1, num2):
+    return num1 * num2
 
-except TypeError as e:
-    print(f'TypeError - Ocurrio un error: {e}, {type(e)}')
+def division(num1,num2):
+    
+    try:
+        return num1/num2
+    except ZeroDivisionError:
+        print("No se puede dividir por cero")
+        return "Operacion erronea"
 
-except Exception as e:
-    print(f'Ocurrio un error: {e}, {type(e)}')
+while True:
+    try:
+        op1= int(input("Ingrese el primer numero:"))
+    
+        op2= int(input("Ingrese el segundo numero: "))
 
-print(f'Resultado: {resultado}')
-print('Continuamos...')
+        break
+
+    except ValueError:
+        print("Los valores introducidos no son correctos. Intentalo de nuevo")
+    
+
+operacion = input('Ingrese la operacion a realizar (suma, resta, multiplicacion,division)')
+
+if operacion =="suma":
+    print(suma(op1,op2))
+elif operacion =="resta":
+    print(resta(op1,op2))
+elif operacion == "multiplicacion":
+    print(multiplicacion(op1,op2))
+elif operacion == "division":
+    print(division(op1,op2))
+else:
+    print("Operacion no comtemplada")
