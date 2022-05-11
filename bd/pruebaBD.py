e6bfe966f7ba1c643bea1b8506a6a2570bfa19de
tomas <viltetomas2003@gmail.com>
2022-05-11 15:36:44 -0300

    Insertando nuevo registro en la base de datos

--- bd/pruebaBD.py
+++ bd/pruebaBD.py
@@ -1,7 +1,9 @@
 import psycopg2
 
 conexion = psycopg2.connect(user ='postgres', password ='admin', host ='localhost', port ='5432', database ='test_db')
-try:
+
+#Haciendo consulta sql y mostrando los registros en la terminal
+"""try:
     with conexion:
         with conexion.cursor() as cursor:
             consulta =" SELECT * FROM persona WHERE id_persona IN %s"
@@ -17,3 +19,20 @@ except Exception as e:
 finally:
 
     cursor.close()
+
+"""
+
+try:
+    with conexion:
+         with conexion.cursor() as cursor:
+             consulta = " INSERT INTO persona (nombre, apellido, email, lenguaje_preferido) VALUES (%s, %s, %s, %s) "
+             valores = ('Michu', 'Vilte', 'michuvilte@gmail.com', 'Solidity')
+             cursor.execute(consulta, valores)
+             registrosInsertados = cursor.rowcount
+             print(f'Registros insertados: {registrosInsertados}')
+except Exception as e:
+    print(f'Ocurrio un error {e}')
+finally:
+    cursor.close()
+
+
