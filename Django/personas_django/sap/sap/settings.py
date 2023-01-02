a7391f13eb90dc66a806c37b0937ee1cd1ad5e9e
Tomas-vilte <88698264+Tomas-vilte@users.noreply.github.com>
2023-01-02 17:15:32 -0300

    Update settings.py

--- Django/personas_django/sap/sap/settings.py
+++ Django/personas_django/sap/sap/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a6oudsw4h6=9blx!l5ph-54ggi(3+)@86(y5bf@m%+hilt(u^%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
